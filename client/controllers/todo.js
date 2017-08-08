9af7f085938b63a055a01a554f8c2585818aaf4d
Jozef Maloch <jozefmaloch@gmail.com>
2017-08-08 20:06:39 +0200

    17. after angular loading the content of tasks, they get their respective classes added to them, so they have different colors for work, personal and completed, removed tasks, etc

--- client/controllers/todo.js
+++ client/controllers/todo.js
@@ -4,48 +4,104 @@ myApp.controller('toDoListController',
 	['$scope', '$http', '$location', '$routeParams',
 	function($scope, $http, $location, $routeParams){
 		
-	$scope.getAllTasks = function() {
-		$http.get('/api/toDoList').then(function(response) {
-			$scope.allTasks = response.data;
-		});
-	}
+		$scope.getAllTasks = function() {
+			$http.get('/api/toDoList').then(function(response) {
+				$scope.allTasks = response.data;
+			});
+		}
 
-	$scope.getTask = function(){
-		var id = $routeParams.id;
-		$http.get('api/toDoList/' + id).then(function(response){
-			$scope.task = response.data;
-		});
-	}
+		$scope.getTask = function(){
+			var id = $routeParams.id;
+			$http.get('api/toDoList/' + id).then(function(response){
+				$scope.task = response.data;
+			});
+		}
 
-	$scope.addTask = function() {
-		$http.post('api/toDoList', $scope.task).then(function(response){
-			window.location.href = '#!/toDoList';
-		});
-	}
+		$scope.addTask = function() {
+			$http.post('api/toDoList', $scope.task).then(function(response){
+				window.location.href = '#!/toDoList';
+			});
+		}
 
-	$scope.editTask = function(){
-		$http.put('api/toDoList/' + task._id, $scope.task).then(function(response){
-			// window.location.href = '#!/toDoList';
-		});
-	}
+		$scope.editTask = function(){
+			$http.put('api/toDoList/' + task._id, $scope.task).then(function(response){
+				// window.location.href = '#!/toDoList';
+			});
+		}
 
-	$scope.completeTask = function(id, task){
-		$http.put('api/toDoList/completed/' + task._id, task).then(function(response){
-			// window.location.href = '#!/toDoList';
-		});
-	}
+		$scope.completeTask = function(id, task){
+			$http.put('api/toDoList/completed/' + task._id, task).then(function(response){
+				// window.location.href = '#!/toDoList';
+			});
+		}
 
-	$scope.removeTask = function(id, task){
-		$http.put('api/toDoList/removed/' + task._id, task).then(function(response){
-			// window.location.href = '#!/toDoList';
-		});
-	}
+		$scope.removeTask = function(id, task){
+			$http.put('api/toDoList/removed/' + task._id, task).then(function(response){
+				// window.location.href = '#!/toDoList';
+			});
+		}
 
-	$scope.deletePermanentlyTask = function(id, task){
-		$http.delete('api/toDoList/deleted/' + id).then(function(response){
-			console.log("lol");
-			//window.location.href = '#!/toDoList';
-		});
-	}
+		$scope.deletePermanentlyTask = function(id, task){
+			$http.delete('api/toDoList/deleted/' + id).then(function(response){
+				console.log("lol");
+				//window.location.href = '#!/toDoList';
+			});
+		}
+		
+		$scope.isLast = function(check) {
+			if(check == true){
+				var li = document.getElementsByTagName("LI");
+				var i;
+				for (i = 6; i < li.length; i++) {
+					switch(li[i].children[0].children[2].innerHTML){
+						case "completed":
+							li[i].classList.add("completed");  
+							break;
+						case "removed":
+							li[i].classList.add("removed");  
+							break;
+					}
+					
+					switch(li[i].children[0].children[1].innerHTML){
+						case "work":
+							li[i].classList.add("work");  
+							li[i].children[0].classList.add("work-content")
+							break;
+						case "personal":
+							li[i].classList.add("personal");  
+							li[i].children[0].classList.add("personal-content")
+							break;
+					}
 
-}]);
+
+				}
+			}
+		};
+	}]);
+
+// fix this pls, it's fking awful, does the same thing n times (n number of tasks)
+/*	myApp.directive('myRepeatDirective', function( $parse ) {
+   		return {
+       		restrict: 'A',
+       		link: function( $scope, elem, attrs ) {    
+				elem.ready(function(){
+					$scope.$apply(function(){
+						console.log("tits");
+						var li = document.getElementsByTagName("LI");
+						var i;
+						console.log("li" + li.length);
+
+						for (i = 6; i < li.length; i++) {
+							console.log(li[i].children[0]);
+							if (li[i].children[0].children[2].innerHTML == "completed") {
+							li[i].classList.toggle('completed');  
+							}
+						console.log(li[i].children[0].classList);
+				
+						}
+					})
+				})
+       		}
+		}
+	})*/
+
