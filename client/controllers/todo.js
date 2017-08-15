2e52a8587c3d82fdd315ea63dcb3fb6fd02aeaad
Jozef Maloch <jozefmaloch@gmail.com>
2017-08-15 20:42:11 +0200

    31. added to calendar different css to dates at which a task has been added

--- client/controllers/todo.js
+++ client/controllers/todo.js
@@ -7,7 +7,18 @@ myApp.controller('toDoListController',
 		$scope.getAllTasks = function() {
 			$http.get('/api/toDoList').then(function(response) {
 				$scope.allTasks = response.data;
+				var eventDates = {};				
+				for (i = 0; i < $scope.allTasks.length; i++) {
+					var taskDate = $scope.allTasks[i].create_date;
+					taskDay = taskDate.slice(8, 10);
+					taskMonth = taskDate.slice(5, 7) - 1;
+					taskYear = taskDate.slice(0, 4);
+					eventDates[ taskDay + taskMonth + taskYear] = new Date(taskYear + "-" + taskMonth + "-" + taskDay );
+				}
+			$scope.test(eventDates);
+				
 			});
+			
 		}
 
 		$scope.getTask = function(){
@@ -105,5 +116,49 @@ myApp.controller('toDoListController',
 				}
 			}		
 		};
+
+		$scope.test = function(eventDates) {
+			$("#dt").datepicker({
+				beforeShowDay: function( date ) {
+				console.log(eventDates);
+				var tities = date.getDate() + "" + date.getMonth() + "" + date.getFullYear();
+				console.log("titie" + tities);
+				date[17] = "2";
+				var highlight = eventDates[tities];
+				console.log("date: " + date + "eventDates[date]: " + eventDates[date]);
+
+				if( highlight ) {
+					
+					return [true, "event", 'Tooltip text'];
+					} else {
+						return [true, '', ''];
+					}
+				},
+				onSelect: function(dateText, inst) {
+        var date = $(this).val();
+        var time = $('#time').val();
+        /*alert('on select triggered');
+        $("#start").val(date + time.toString(' HH:mm').toString());*/
+        var month = date.slice(0, 2);
+        var day = date.slice(3, 5);
+        var year = date.slice(6, 10);        
+        var li = document.getElementsByTagName("LI");
+        var i;
+
+        for (i = 6; i < li.length; i++) {
+            var taskDate = li[i].children[1].children[3].innerHTML;
+            taskDay = taskDate.slice(8, 10);
+            taskMonth = taskDate.slice(5, 7);
+            taskYear = taskDate.slice(0, 4);
+            if (day == taskDay && month == taskMonth && year == taskYear) {
+                li[i].style.display = "block";
+            }
+            else {
+                li[i].style.display = "none";                
+            }
+        }
+    }    
+			});
+		}
 	}]);
 
